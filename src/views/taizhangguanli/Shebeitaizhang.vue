641c56ae4e23b10e5072123c67e0635f0d0ff15b
zhouxuanyou <1102587121@qq.com>
2019-05-13 19:37:26 +0800

    电力运维demo 台账管理前后端交互

--- src/views/taizhangguanli/Shebeitaizhang.vue
+++ src/views/taizhangguanli/Shebeitaizhang.vue
@@ -1,53 +1,71 @@
 <template>
     <div class="shebeitaizhang">
-        <el-row>
-            <el-col :span="4"><div class="grid-content bg-purple">
-                <el-tree
-                        :data="data"
-                        :props="defaultProps"
-                        accordion
-                        @node-click="handleNodeClick">
-                </el-tree>
-            </div></el-col>
-            <el-col :span="20"><div class="grid-content bg-purple-light">
-
+        <el-row :gutter="20">
+            <el-col :span="8"><div class="grid-content bg-purple">
+                <!--站点-->
                 <el-card class="box-card">
                     <div slot="header" class="clearfix">
-                        <span>卡片名称</span>
-                        <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+                        <span>站点</span>
+                        <el-button style="float: right; padding: 3px 0" type="text" @click="centerDialogVisible = true">新增</el-button>
                     </div>
-                    <el-table
-                            :data="tableData"
-                            style="width: 100%">
-                        <el-table-column
-                                prop="date"
-                                label="日期"
-                                width="180">
-                        </el-table-column>
-                        <el-table-column
-                                prop="name"
-                                label="姓名"
-                                width="180">
-                        </el-table-column>
-                        <el-table-column
-                                prop="address"
-                                label="地址">
-                        </el-table-column>
-                        <el-table-column label="操作">
-                            <template slot-scope="scope">
-                                <el-button
-                                        size="mini"
-                                        @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-                                <el-button
-                                        size="mini"
-                                        type="danger"
-                                        @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-                            </template>
-                        </el-table-column>
-                    </el-table>
+                    <el-tree :data="data"
+                             :props="defaultProps"
+                             @node-click="handleNodeClick"
+                             node-key="id"
+                             ref="tree"
+                             :highlight-current = "true"
+                    ></el-tree>
+                </el-card>
+            </div></el-col>
+            <el-col :span="8"><div class="grid-content bg-purple">
+                <!--机柜-->
+                <el-card class="box-card">
+                    <div slot="header" class="clearfix">
+                        <span>机柜</span>
+                        <el-button style="float: right; padding: 3px 0" type="text">新增</el-button>
+                    </div>
+                    <el-tree :data="jiguidata" :props="defaultprop" @node-click="handleNodeClicks"></el-tree>
+                </el-card>
+            </div></el-col>
+            <el-col :span="8"><div class="grid-content bg-purple">
+                <!--设备-->
+                <el-card class="box-card">
+                    <div slot="header" class="clearfix">
+                        <span>设备</span>
+                        <el-button style="float: right; padding: 3px 0" type="text">新增</el-button>
+                    </div>
+                    <el-tree :data="shebei" :props="defaultprops" ></el-tree>
                 </el-card>
             </div></el-col>
         </el-row>
+
+        <!--站点增加模态框-->
+        <el-dialog
+                title="提示"
+                :visible.sync="centerDialogVisible"
+                width="30%"
+                center>
+            <el-form ref="form" :model="form" label-width="80px">
+                <el-form-item label="站房">
+                    <el-input v-model="form.name"></el-input>
+                </el-form-item>
+                <el-form-item label="站点区域">
+                    <el-select v-model="form.region" placeholder="请选择活动区域" value="">
+                        <!--<el-option label="区域一" value="shanghai"></el-option>-->
+                        <el-option v-for="(res,index) in data" :label="res.stationName" :value="res.stationName" :key="index"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="onSubmit">立即创建</el-button>
+                    <el-button @click="centerDialogVisible = false">取消</el-button>
+                </el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+    <!--<el-button @click="centerDialogVisible = false">取 消</el-button>-->
+    <!--<el-button type="primary" @click="centerDialogVisible = false">确 定</el-button>-->
+  </span>
+        </el-dialog>
+
 
     </div>
 </template>
@@ -57,74 +75,73 @@
         name: "Shebeitaizhang",
         data() {
             return {
-                data: [{
-                    label: '一级 1',
-                    children: [{
-                        label: '二级 1-1',
-                        children: [{
-                            label: '三级 1-1-1'
-                        }]
-                    }]
-                }, {
-                    label: '一级 2',
-                    children: [{
-                        label: '二级 2-1',
-                        children: [{
-                            label: '三级 2-1-1'
-                        }]
-                    }, {
-                        label: '二级 2-2',
-                        children: [{
-                            label: '三级 2-2-1'
-                        }]
-                    }]
-                }, {
-                    label: '一级 3',
-                    children: [{
-                        label: '二级 3-1',
-                        children: [{
-                            label: '三级 3-1-1'
-                        }]
-                    }, {
-                        label: '二级 3-2',
-                        children: [{
-                            label: '三级 3-2-1'
-                        }]
-                    }]
-                }],
+                centerDialogVisible: false,
+                data: [
+                    ],
+                jiguidata:[],
+                shebei:[],
+                defaultprop:{
+                    children: 'children',
+                    label: 'cabinetname'
+
+                },
                 defaultProps: {
                     children: 'children',
-                    label: 'label'
+                    label: 'stationName'
                 },
-                tableData: [{
-                    date: '2019-05-10',
-                    name: '开能智慧能源有限公司',
-                    address: '成都市武侯区人民南路44号首座C栋18'
-                }, {
-                    date: '2019-05-10',
-                    name: '开能智慧能源有限公司',
-                    address: '成都市武侯区人民南路44号首座C栋18'
-                }, {
-                    date: '2019-05-10',
-                    name: '开能智慧能源有限公司',
-                    address: '成都市武侯区人民南路44号首座C栋18'
-                }, {
-                    date: '2019-05-10',
-                    name: '开能智慧能源有限公司',
-                    address: '成都市武侯区人民南路44号首座C栋18'
-                }]
-            }
+                defaultprops:{
+                    children: 'children',
+                    label: 'equipmentname'
+                },
+                form: {
+                    name: '',
+                    region: '',
+                }
+            };
+
+        },
+        created(){
+            this.gettaizhanglist();
 
         },
         methods: {
+            //获取机柜信息
             handleNodeClick(data) {
-                console.log(data);
+                // console.log(data.id);
+                let stationid = data.id;
+                this.req.get('/cabinet/list',{stationid})
+                    .then(res=>{
+                        console.log(res);
+                        this.jiguidata = res;
+                    })
             },
-            handleEdit(index, row) {
-                console.log(index, row);
+            //获取设备信息
+            handleNodeClicks(data) {
+                // console.log(data.id);
+                let cabinetId = data.id;
+                this.req.get('equipment/list',{cabinetId})
+                    .then(res=>{
+                        console.log(res);
+                        this.shebei = res;
+                    })
             },
-            handleDelete(index, row) {
-                console.log(index, row);
+            getCheckedNodes() {
+                console.log(this.$refs.tree.getCheckedNodes());
+            },
+            //获取站点信息
+            gettaizhanglist(){
+                this.req.get('/stationBuilding/list')
+                    .then(res=>{
+                        this.data = res;
+
+                    })
+                    .catch(err=>{
+                        console.log(err)
+                    })
+
+            },
+            onSubmit() {
+                console.log(this.form);
             }
         }
 
@@ -136,24 +153,21 @@
 
 <style scoped lang="less">
     .shebeitaizhang{
+        padding: 20px;
         height: 100%;
         .el-row{
             height: 100%;
             .el-col{
                 height: 100%;
-                .bg-purple{
-                    height: 100%;
-                    background: #000;
-                    .el-tree{
-                        height: 100%;
-                    }
+                .clearfix:before,
+                .clearfix:after {
+                    display: table;
+                    content: "";
                 }
-                .bg-purple-light{
-                    height: 100%;
-                    .box-card{
-                        height: 100%;
-                    }
+                .clearfix:after {
+                    clear: both
                 }
+
             }
         }
     }
