4600457263f855b2aa1bd20da03d4f1b629dcb0d
zhouxuanyou <1102587121@qq.com>
2019-05-14 17:48:42 +0800

    电力运维demo 角色管理

--- src/views/xitongshezhi/Yonghuguanli.vue
+++ src/views/xitongshezhi/Yonghuguanli.vue
@@ -8,16 +8,16 @@
                     :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
                     style="width: 100%">
                 <el-table-column
-                        label="创建时间"
-                        prop="date">
+                        label="加入时间"
+                        prop="joinTime">
                 </el-table-column>
                 <el-table-column
                         label="用户名"
-                        prop="name">
+                        prop="username">
                 </el-table-column>
                 <el-table-column
-                        label="用户身份"
-                        prop="identity">
+                        label="地址"
+                        prop="address">
                 </el-table-column>
                 <el-table-column
                         align="right">
@@ -50,9 +50,10 @@
                     width="30%"
                     center>
                 <div class="item" :data="userinfo">
-                    <p>用户名：<span>{{userinfo.name}}</span></p>
-                    <p>创建时间：<span>{{userinfo.date}}</span></p>
-                    <p>用户身份：<span>{{userinfo.identity}}</span></p>
+                    <p>用户名：<span>{{userinfo.username}}</span></p>
+                    <p>创建时间：<span>{{userinfo.joinTime}}</span></p>
+                    <p>用户身份：<span>{{userinfo.leader}}</span></p>
+                    <p>联系电话：<span>{{userinfo.phoneNumber}}</span></p>
                 </div>
 
                 <span slot="footer" class="dialog-footer">
@@ -62,10 +63,17 @@
             </el-dialog>
             <!--修改模态框-->
             <el-dialog
-                    title="角色信息"
+                    title="角色管理"
                     :visible.sync="flag"
-                    width="30%"
+                    width="50%"
                     center>
+                <el-transfer
+                        v-model="value"
+                        :data="jiaosedata"
+                        :titles="['所有角色','当前角色']"
+
+                >
+                </el-transfer>
 
                 <span slot="footer" class="dialog-footer">
     <el-button @click="flag = false">取 消</el-button>
@@ -80,43 +88,69 @@
     export default {
         name: "Danweiguanli",
         data() {
+            // const generateData = _ => {
+            //     const data = [];
+            //     for (let i = 1; i <= 15; i++) {
+            //         data.push({
+            //             key: i,
+            //             label: `备选项 ${ i }`,
+            //             disabled: i % 4 === 0
+            //         });
+            //     }
+            //     return data;
+            // };
             return {
                 userinfo: {
-                    date: '2019-05-10',
-                    name: '张三',
-                    identity: '超级管理员'
+
                 },
                 search: '',
                 centerDialogVisible: false,
                 flag:false,
-                tableData: [{
-                    date: '2019-05-10',
-                    name: '张三',
-                    identity: '超级管理员'
-                }, {
-                    date: '2019-05-10',
-                    name: '李四',
-                    identity: '管理员'
-                }, {
-                    date: '2019-05-10',
-                    name: '王五',
-                    identity: '工程师'
-                }, {
-                    date: '2019-05-10',
-                    name: '小周周',
-                    identity: '普通用户'
-                }],
+                tableData: [],
+                jiaosedata:[],
+                value:[],
             };
 
         },
         methods: {
             handleEdit(index, row) {
-                console.log(index, row);
+                // console.log(index, row);
+                this.req.get('role/list')
+                    .then(res=>{
+                        res.forEach(jiaose=>{
+                            // console.log(jiaose.roleName);
+                            this.jiaosedata.push({
+                                lable:jiaose.roleName,
+                                key:jiaose.id,
+                            })
+                        });
+                        // console.log(this.jiaosedata)
+                        // console.log(res)
+                    })
+                    .catch(err=>{
+                        console.log(err)
+                    })
             },
             handleSee(index, row) {
-                console.log(index, row);
+                // console.log(index, row);
+                this.userinfo = this.tableData[index];
+                // console.log(this.userinfo);
+            },
+            //获取用户信息
+            getuserlist(){
+                this.req.get('powerunit/list')
+                    .then(res=>{
+                        // console.log(res);
+                        this.tableData = res;
+                    })
+                    .catch(err=>{
+                        console.log(err)
+                    })
             }
         },
+        created(){
+            this.getuserlist();
+        }
     }
 </script>
 
--- src/views/yunweizhongxin/Shishishuju.vue
+++ src/views/yunweizhongxin/Shishishuju.vue
@@ -1,13 +1,246 @@
 <template>
-    <div>实时数据</div>
+    <div class="shishishuju">
+        <el-row>
+            <el-col :span="4"><div class="grid-content bg-purple">
+                <el-input
+                        placeholder="输入关键字进行过滤"
+                        v-model="filterText"
+                >
+                </el-input>
+
+                <el-tree
+                        class="filter-tree"
+                        :data="data"
+                        :props="defaultProps"
+                        default-expand-all
+                        :filter-node-method="filterNode"
+                        ref="tree2">
+                </el-tree>
+            </div></el-col>
+            <el-col :span="20"><div class="grid-content bg-purple-light">
+                <!--实时曲线-->
+                <div id="container" style="height: 50%"></div>
+            </div></el-col>
+        </el-row>
+    </div>
 </template>
 
 <script>
+    import echarts from 'echarts';
+    import req from '../../api/request'
     export default {
-        name: "Shishishuju"
+        name: "Shishishuju",
+        watch: {
+            filterText(val) {
+                this.$refs.tree2.filter(val);
+            }
+        },
+        created(){
+            this.gettaizhanglist();
+        },
+
+        methods: {
+            filterNode(value, data) {
+                if (!value) return true;
+                return data.label.indexOf(value) !== -1;
+            },
+            //获取站点信息
+
+            gettaizhanglist(){
+                let cabinetId =2;
+                this.req.get('/equipment/list',{cabinetId})
+                    .then(res=>{
+                        // console.log(res);
+                        this.data = res;
+
+                    })
+                    .catch(err=>{
+                        console.log(err)
+                    })
+
+            },
+        },
+        mounted(){
+            this.gettaizhanglist();
+            var dom = document.getElementById("container");
+            var myChart = echarts.init(dom);
+            var app = {};
+            var option = null;
+            option = {
+                title: {
+                    text: '折线图堆叠'
+                },
+                //提示框信息
+                tooltip: {
+                    trigger: 'axis'
+                },
+                legend: {
+                    data:['A相电流','B相电流','C相电流']
+                },
+                //绘图网格
+                grid: {
+                    left: '3%',
+                    right: '4%',
+                    bottom: '3%',
+                    containLabel: true
+                },
+                toolbox: {
+                    show: true,
+                    feature: {
+                        dataZoom: {
+                            yAxisIndex: 'none'
+                        },
+                        dataView: {readOnly: false},
+                        magicType: {type: ['line', 'bar']},//切换图像
+                        restore: {},
+                        saveAsImage: {}
+                    }
+                },
+                //x坐标轴配置
+                xAxis: {
+                    type: 'category',
+                    boundaryGap: true,
+                    data: (function (){
+                        var now = new Date();
+                        var res = [];
+                        var len = 10;
+                        while (len--) {
+                            res.unshift(now.toLocaleTimeString().replace(/^\D*/,''));
+                            now = new Date(now - 3000);
+                        }
+                        return res;
+                    })()
+                },
+                yAxis: {
+                    type: 'value'
+                },
+                series: [
+                    {
+                        name:'A相电流',
+                        type:'line',
+                        stack: '总量',
+                        data:(function (){
+                            var res = [];
+                            var len = 0;
+                            while (len < 10) {
+                                res.push((Math.random()*10 + 5).toFixed(1) - 0);
+                                len++;
+                            }
+                            return res;
+                        })(),
+                        markLine: {
+                            silent: true,
+                            data: [{
+                                yAxis: 10
+                            }]
+                        }
+                    },
+                    {
+                        name:'B相电流',
+                        type:'line',
+                        stack: '总量',
+                        data:(function (){
+                            var res = [];
+                            var len = 0;
+                            while (len < 10) {
+                                res.push((Math.random()*10 + 5).toFixed(1) - 0);
+                                len++;
+                            }
+                            return res;
+                        })(),
+                        markLine: {
+                            silent: true,
+                            data: [{
+                                yAxis: 20
+                            }]
+                        }
+
+                    },
+                    {
+                        name:'C相电流',
+                        type:'line',
+                        stack: '总量',
+                        data:(function (){
+                            var res = [];
+                            var len = 0;
+                            while (len < 10) {
+                                res.push((Math.random()*10 + 5).toFixed(1) - 0);
+                                len++;
+                            }
+                            return res;
+                        })(),
+                        markLine: {
+                            silent: true,
+                            data: [{
+                                yAxis: 25
+                            }]
+                        }
+                    },
+                ]
+            };
+
+            app.count = 11;
+            let deviceId =2;
+            setInterval(function (){
+               let axisData = (new Date()).toLocaleTimeString().replace(/^\D*/,'');
+
+                var data1 = option.series[0].data;
+                data1.shift();
+                data1.push((Math.random() * 10 + 5).toFixed(1) - 0);
+
+                option.xAxis.data.shift();
+                option.xAxis.data.push(axisData);
+               //  req.get('/realTimeData/getValue',{deviceId})
+               //      .then(res=>{
+               //          console.log(res);
+               //      })
+               //      .catch(err=>{
+               //          console.log(err);
+               //      })
+
+                myChart.setOption(option);
+            }, 2100);
+
+        },
+
+        data() {
+            return {
+                filterText: '',
+                data: [],
+                defaultProps: {
+                    children: 'children',
+                    label: 'equipmentname'
+                }
+            };
+        },
+
+
     }
 </script>
 
-<style scoped>
+<style scoped lang="less">
+    .shishishuju{
+        height: 100%;
+        .el-row{
+            height: 100%;
+            .el-col{
+                height: 100%;
+                .bg-purple{
+                    height: 100%;
+                    .el-input{
+                        background: #ccc;
+                    }
+                    .filter-tree{
+                        height: 100%;
+                        background: #ccc;
+                    }
+                }
+                .bg-purple-light{
+                    height: 100%;
+                    background: #fff;
+                }
+            }
+        }
 
+    }
 </style>
